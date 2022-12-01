a4b7f5fc21563ab0b5f3cda9c761a24b9d8744f5
random <random@ipsa.fr>
2022-12-01 11:11:33 +0000

    fichier perfect ajoute

---
+++ perfect.py
@@ -0,0 +1,10 @@
+
+def plot_cosinus():
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    angle = np.arange(360)
+    cosinus = np.cos(2 * np.pi * angle /360)
+
+    plt.plot(angle, cosinus)    
+    
