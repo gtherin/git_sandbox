125052389633aab6fca2f76cff214f70664b1390
random <random@ipsa.fr>
2022-12-01 11:11:33 +0000

    fichier perfect ajoute

--- bugger.py
+++ bugger.py
@@ -1,3 +1,4 @@
+
 def plot_sinus():
     import numpy as np
     import matplotlib.pyplot as plt
@@ -5,4 +6,4 @@ def plot_sinus():
     angle = np.arange(360)
     sinus = np.sin(2 * np.pi * angle /360)
 
-    plt.plot(angle, sinnus)
+    plt.plot(angle, sinus) 
